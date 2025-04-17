6f6669050af7555c418ebdf2595a8795014a6093
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-04-17 16:00:38 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    if (n > 1) {
+        dp[2] = 2;
+    }
+
+    for (int i = 3; i <= n; ++i) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n] % 10007 << endl;
+
+    return 0;
+}
